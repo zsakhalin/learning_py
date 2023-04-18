93c1fcbabe8d2533a6de5cf6bdfc16f2d7da06ff
Alex <gittraveler@gmail.com>
2023-04-18 17:37:43 +0400

    edit 6_set

--- book_tutorials/book-self-taught-programmer/chapt5/6_set.py
+++ book_tutorials/book-self-taught-programmer/chapt5/6_set.py
@@ -2,7 +2,7 @@
 set1 = set("qwerty")
 print(set1)
 try:
-    print(set1[0]) #set is not subscriptable
+    print(set1[0]) #incorrect, set is not subscriptable
 except TypeError:
     print("TypeError: 'set' object is not subscriptable")
     print(type(set1))
