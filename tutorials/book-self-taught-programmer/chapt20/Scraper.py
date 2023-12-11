80993ddcd9a360ddf809feda5d34e6cf21aa7273
Alex <gittraveler@gmail.com>
2023-12-11 04:24:55 +0400

    add chapt20 Google News parser

---
+++ tutorials/book-self-taught-programmer/chapt20/Scraper.py
@@ -0,0 +1,24 @@
+import urllib.request
+from bs4 import BeautifulSoup
+
+class Scraper:
+    def __init__(self, url):
+        self.url = url
+
+    def scrape(self):
+        r = urllib.request.urlopen(self.url) # return site's html-code with adress url
+        html = r.read() # html has site's code
+        parser = "html.parser"
+        sp = BeautifulSoup(html, parser) # parses html-code
+        with open('urls', 'w') as f:
+            for tag in sp.find_all("a"):
+                url = tag.get("href")
+                if url is None:
+                    continue
+                if "html" in url:
+                    f.write(url + "\n")
+                    print(f"\n {url}")
+
+print("In progress...")
+url: str = "https://news.google.com/"
+Scraper(url).scrape()
