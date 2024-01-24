822816f5159d6ccf569fe61870082feb33c77421
Alex <gittraveler@gmail.com>
2024-01-24 20:56:52 +0400

    add task from 5th day of 100days Udemy course

---
+++ tutorials/100days/5th day/password generator.py
@@ -0,0 +1,21 @@
+#Password Generator
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+password = []
+for letter in range(1, nr_letters + 1):
+    password.append(random.choice(letters))
+for symbol in range(1, nr_symbols + 1):
+    password.append(random.choice(symbols))
+for number in range(1, nr_numbers + 1):
+    password.append(random.choice(numbers))
+random.shuffle(password)   # shuffling list
+# password = random.sample(password, len(password))   # another way to shuffling list
+password = ''.join(password)
+print(f"New password: {password}\nIts length is " + str(len(password)))
